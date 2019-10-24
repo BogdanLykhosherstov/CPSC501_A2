f3b5b99f2b524253559316462c89ac2a34dfa47c
Bogdan Lykhosherstov <bogdan@exdee.ai>
2019-10-23 18:49:02 -0600

    Implemented recursive support for 1-4 (of 6) requirements. Added unit tests for 1-4. TODO (Refactor format printing for 4)

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Inspector {
@@ -9,32 +10,76 @@ public class Inspector {
         inspectClass(c, obj, recursive, 0);
     }
 
+    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+
+        //for each member, get Name, Superclass, Interface, etc.
+        getClassName(c,depth);
+        getSuperClassInfo(c,obj,recursive,depth);
+        getInterfaceInfo(c,obj,recursive,depth);
+        getConstructorInfo(c,obj,recursive,depth);
+
+    }
+
 
     public void properPrint(String text, int depth){
         for(int i=0;i<depth;i++){
-            System.out.println('|'+'\t');
+            System.out.print("\t");
         }
         System.out.println(text);
 
     }
     //returns current class name
     public void getClassName(Class c, int depth){
-        properPrint("Name of class: "+c.getName(),depth);
+        properPrint("Name: "+c.getName(),depth);
     }
     //get super class info
-    public void getSuperClassInfo(){
+    public void getSuperClassInfo(Class c, Object obj, boolean recursive, int depth){
 
-    }
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-        getClassName(c,depth);
+        //base case
+        if(c.equals(Object.class))return;
 
-        System.out.println("Immediate Super Class: "+c.getSuperclass().getName());
-        System.out.println("Interfaces: "+Arrays.toString(c.getInterfaces()));
-        System.out.println("Constructors: ");
-        for (Constructor con : c.getConstructors()){
-            System.out.println(" * Name: "+con.getName());
-            System.out.println(" -- Parameter Types: "+ Arrays.toString(con.getParameterTypes()));
-            System.out.println(" -- Modifiers: "+ Modifier.toString(con.getModifiers()));
+        Class super_class = c.getSuperclass();
+
+        if(super_class!=null){
+            properPrint("Super Class: "+super_class.getName(),depth);
+            inspectClass(super_class,obj,recursive,depth+1);
+        }
+        else{
+            properPrint("Super Class: None",depth);
         }
     }
+    // get constructors
+    public void getConstructorInfo(Class c, Object obj, boolean recursive, int depth){
+        //interfaces cannot be instantiated
+        if(c.isInterface()){
+            properPrint("Constructors: None",depth);
+        }
+        else{
+            properPrint("Constructors: ",depth);
+            for (Constructor con : c.getConstructors()){
+                properPrint(" * Name: "+con.getName(),depth);
+                properPrint(" -- Parameter Types: "+ Arrays.toString(con.getParameterTypes()),depth);
+                properPrint(" -- Modifiers: "+ Modifier.toString(con.getModifiers()),depth);
+            }
+        }
+
+    }
+    //get interfaces info
+    public void getInterfaceInfo(Class c, Object obj, boolean recursive, int depth){
+
+        Class[] interfaceList = c.getInterfaces();
+        if(interfaceList.length == 0){
+            properPrint("Interface: None",depth);
+        }
+        else{
+            for(Class interFace : interfaceList){
+                properPrint("Interface: "+interFace.getName(),depth);
+                inspectClass(interFace,obj,recursive,depth+1);
+            }
+        }
+
+
+    }
+
+
 }
--- test/Tests.java
+++ test/Tests.java
@@ -1,15 +1,57 @@
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Tests{
 
-    Inspector ins = new Inspector();
+    private static ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private Inspector ins = new Inspector();
+
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restore() {
+        output.reset();
+    }
+
     @Test
-    public void Inspector_Test() throws Exception {
+    public void Get_ClassName() throws Exception{
         ClassB b1 = new ClassB();
-        ins.inspect(b1,false);
+        ins.getClassName(b1.getClass(),0);
+        assert(output.toString().contains("Name: ClassB"));
+    }
+    @Test
+    public void Get_SuperClass() throws Exception{
+        ClassA a1 = new ClassA();
+        ins.getSuperClassInfo(a1.getClass(),null,true,0);
+        assert(output.toString().contains("Super Class: java.lang.Object"));
+    }
+    @Test
+    public void Get_Interface() throws Exception{
+        ClassB b1 = new ClassB();
+        ins.getInterfaceInfo(b1.getClass(),null,true,0);
+        assert(output.toString().contains("Interface: java.lang.Runnable"));
+    }
+    @Test
+    public void Get_ConstructorInfo() throws Exception{
+        ClassB b1 = new ClassB();
+        ins.getConstructorInfo(b1.getClass(),null,true,0);
+
+        assert(output.toString().contains(" * Name: ClassB"));
+        assert(output.toString().contains(" -- Parameter Types: []"));
+        assert(output.toString().contains(" -- Modifiers: public"));
+
     }
 
 }
