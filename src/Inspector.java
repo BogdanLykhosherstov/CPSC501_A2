a8dc55ac9b4890df01f67a38d611c0564853b94c
Bogdan Lykhosherstov <bogdan@exdee.ai>
2019-10-25 01:21:01 -0600

    Finished 1-6. Refactoring(Extract method arrayInspection and referenceInspection) and unit testing left.

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,7 +16,8 @@ public class Inspector {
         getSuperClassInfo(c,obj,recursive,depth);
         getInterfaceInfo(c,obj,recursive,depth);
         getConstructorInfo(c,obj,recursive,depth);
-
+        getMethodsInfo(c,obj,recursive,depth);
+        getFieldsInfo(c,obj,recursive,depth);
     }
 
 
@@ -28,6 +28,118 @@ public class Inspector {
         System.out.println(text);
 
     }
+
+    // get methods info
+    public void getMethodsInfo(Class c, Object obj, boolean recursive, int depth){
+        //interfaces cannot be instantiated
+        if(c.isInterface()){
+            properPrint("Methods: None",depth);
+        }
+        else{
+            properPrint("Methods: ",depth);
+            for (Method meth : c.getDeclaredMethods()){
+                properPrint(" Name: "+meth.getName(),depth);
+                properPrint("Parameter Types: "+ Arrays.toString(meth.getParameterTypes()),depth+1);
+                properPrint("Modifiers: "+ Modifier.toString(meth.getModifiers()),depth+1);
+                properPrint("Return Type: "+ meth.getReturnType(),depth+1);
+                properPrint("Exceptions: "+ Arrays.toString(meth.getExceptionTypes()),depth+1);
+            }
+
+        }
+
+    }
+    // get fields info
+    //REFACTOR: pull out if recurse stuff out of this method into its own, same with inspect array
+    public void getFieldsInfo(Class c, Object obj, boolean recursive, int depth){
+        //interfaces cannot be instantiated
+        if(c.isInterface()){
+            properPrint("Fields: None",depth);
+        }
+        else{
+            properPrint("Fields: ",depth);
+            for (Field field : c.getDeclaredFields()){
+                properPrint(" Name: "+ field.getName(),depth);
+                properPrint("Type: "+ field.getType(),depth+1);
+                properPrint("Modifiers: "+ Modifier.toString(field.getModifiers()),depth+1);
+
+                //access private fields
+                Object value = null;
+                field.setAccessible(true);
+                try{
+                    value = field.get(obj);
+                }
+                catch (IllegalAccessException e){
+                    System.out.println("ERROR: Cannot access field");
+                }
+                //- if field inaccessible
+                if(value==null){
+                    properPrint("Value: Null",depth+1);
+                }
+                //1. if primitive, just print as is
+                else if(field.getType().isPrimitive()){
+                    properPrint("Value: "+value, depth+1);
+                }
+                //2. if array - print reference (pull up into arrayInspection later)
+                else if(field.getType().isArray()){
+                    if(recursive){
+                        //inspect each object of the array
+                        Class componentType  = value.getClass().getComponentType();
+                        properPrint("Array Type: "+componentType,depth+1);
+                        int length = Array.getLength(value);
+                        properPrint("Array Length: "+length,depth+1);
+
+                        //arrays could contain primitives, other arrays, or objects
+                        for(int i=0;i<length;i++){
+                            Object arrayItem = Array.get(value,i);
+                            if(arrayItem==null){
+                                properPrint("Array Value: Null",depth+1);
+                            }
+                            //1. primitive
+                            if(componentType.isPrimitive()){
+                                //pull up into method later: referenceInspection
+                               properPrint("Array Value: "+arrayItem,depth+1);
+                            }
+                            //2. array (print value for now)
+                            if(componentType.isArray()){
+                                properPrint("Array Value: "+arrayItem,depth+1);
+                            }
+                            //3. object
+                            else{
+                                //pull up into method later: referenceInspection
+                                if(recursive){
+                                    inspectClass(value.getClass(),value,recursive,depth+1);
+                                }
+                                else{
+                                    properPrint("Value: "+value.getClass().getName()+"@"+value.getClass().hashCode(),depth+1);
+                                }
+                            }
+                        }
+
+                    }
+                    //recursive off - just print the value of array
+                    else{
+                        properPrint("Value: "+value.getClass().getName()+"@"+value.getClass().hashCode(),depth+1);
+                    }
+                }
+                //3. else its an object, print reference value
+                else{
+                    if(recursive){
+                        inspectClass(value.getClass(),value,recursive,depth+1);
+                    }
+                    else{
+                        properPrint("Value: "+value.getClass().getName()+"@"+value.getClass().hashCode(),depth+1);
+                    }
+                }
+                //
+
+
+            }
+
+        }
+
+    }
+
+
     //returns current class name
     public void getClassName(Class c, int depth){
         properPrint("Name: "+c.getName(),depth);
@@ -57,9 +169,9 @@ public class Inspector {
         else{
             properPrint("Constructors: ",depth);
             for (Constructor con : c.getConstructors()){
-                properPrint(" * Name: "+con.getName(),depth);
-                properPrint(" -- Parameter Types: "+ Arrays.toString(con.getParameterTypes()),depth);
-                properPrint(" -- Modifiers: "+ Modifier.toString(con.getModifiers()),depth);
+                properPrint(" Name: "+con.getName(),depth);
+                properPrint("Parameter Types: "+ Arrays.toString(con.getParameterTypes()),depth+1);
+                properPrint("Modifiers: "+ Modifier.toString(con.getModifiers()),depth+1);
             }
         }
 
